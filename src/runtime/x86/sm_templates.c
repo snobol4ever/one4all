b52fc91a80171ce5c80a50d4767613fb8596bd9a
LCherryholmes <lcherryh@yahoo.com>
2026-05-13 01:36:36 +0000

    Inline emit_sm_rtcall/pat_lbl_rt/lbl_rt wrappers; delete helpers
    The 22 emit_sm_pat_X() one-liners and 3 emit_sm_pat_lbl_rt() one-liners
    wrapped trivial bodies behind string arguments that duplicated the
    function name. Inlined the 4-line body into each function directly and
    deleted the three static helper functions.
    
    sm_templates.c: 360 -> 327 lines.
    Gates: smoke 7/7, template-byte-id 4/4, snocone 5/5.

--- src/runtime/x86/sm_templates.c
+++ src/runtime/x86/sm_templates.c
@@ -3,12 +3,6 @@
 #include "templates.h"
 #include "sm_prog.h"
 
-static void emit_sm_rtcall    (const char *macro, const char *rt_sym);
-static void emit_sm_lbl_rt    (const char *macro, const char *rt_sym, const char *lbl, uint64_t ptr);
-static void emit_sm_pat_lbl_rt(const char *macro, const char *rt_sym, const char *lbl, uint64_t ptr);
-static void make_pc_label(bb_label_t *lbl, int target_pc);
-
-/*---- SM_ARITH family (ADD/SUB/MUL/DIV/MOD) -----------------------------------------------------------------------*/
 void emit_sm_arith_op(int op_enum, const char *macro_name)
 {
     emit_macro_begin(macro_name ? macro_name : "ARITH", NULL, 0);
@@ -262,63 +256,63 @@ void emit_sm_pat_capture_fn_args(const char *fname_lbl, uint64_t fname_ptr,
     emit_pad_to_blob_size();
 }
 /*====================================================================================================================*/
-void emit_sm_pat_lit      (const char *lbl, uint64_t ptr) { emit_sm_pat_lbl_rt("PAT_LIT",      "rt_pat_lit",      lbl, ptr); }
-void emit_sm_pat_refname  (const char *lbl, uint64_t ptr) { emit_sm_pat_lbl_rt("PAT_REFNAME",  "rt_pat_refname",  lbl, ptr); }
-void emit_sm_pat_usercall (const char *lbl, uint64_t ptr) { emit_sm_pat_lbl_rt("PAT_USERCALL", "rt_pat_usercall", lbl, ptr); }
+void emit_sm_pat_lit(const char *lbl, uint64_t ptr)      { static const char *const p[] = {"lbl"}; emit_macro_begin("PAT_LIT", p, 1); emit_lea_rdi_strtab_sym(lbl, ptr); bb_emit_call_sym_plt("rt_pat_lit", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_refname(const char *lbl, uint64_t ptr)  { static const char *const p[] = {"lbl"}; emit_macro_begin("PAT_REFNAME", p, 1); emit_lea_rdi_strtab_sym(lbl, ptr); bb_emit_call_sym_plt("rt_pat_refname", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_usercall(const char *lbl, uint64_t ptr) { static const char *const p[] = {"lbl"}; emit_macro_begin("PAT_USERCALL", p, 1); emit_lea_rdi_strtab_sym(lbl, ptr); bb_emit_call_sym_plt("rt_pat_usercall", 0); emit_macro_end(); emit_pad_to_blob_size(); }
 /*====================================================================================================================*/
-void emit_sm_pat_eps    () { emit_sm_rtcall("PAT_EPS",     "rt_pat_eps");     }
-void emit_sm_pat_arb    () { emit_sm_rtcall("PAT_ARB",     "rt_pat_arb");     }
-void emit_sm_pat_rem    () { emit_sm_rtcall("PAT_REM",     "rt_pat_rem");     }
-void emit_sm_pat_fail   () { emit_sm_rtcall("PAT_FAIL",    "rt_pat_fail");    }
-void emit_sm_pat_succeed() { emit_sm_rtcall("PAT_SUCCEED", "rt_pat_succeed"); }
-void emit_sm_pat_abort  () { emit_sm_rtcall("PAT_ABORT",   "rt_pat_abort");   }
-void emit_sm_pat_bal    () { emit_sm_rtcall("PAT_BAL",     "rt_pat_bal");     }
-void emit_sm_pat_fence  () { emit_sm_rtcall("PAT_FENCE",   "rt_pat_fence");   }
-void emit_sm_pat_fence1 () { emit_sm_rtcall("PAT_FENCE1",  "rt_pat_fence1");  }
-void emit_sm_pat_span   () { emit_sm_rtcall("PAT_SPAN",    "rt_pat_span");    }
-void emit_sm_pat_break  () { emit_sm_rtcall("PAT_BREAK",   "rt_pat_break");   }
-void emit_sm_pat_any    () { emit_sm_rtcall("PAT_ANY",     "rt_pat_any");     }
-void emit_sm_pat_notany () { emit_sm_rtcall("PAT_NOTANY",  "rt_pat_notany");  }
-void emit_sm_pat_len    () { emit_sm_rtcall("PAT_LEN",     "rt_pat_len");     }
-void emit_sm_pat_pos    () { emit_sm_rtcall("PAT_POS",     "rt_pat_pos");     }
-void emit_sm_pat_rpos   () { emit_sm_rtcall("PAT_RPOS",    "rt_pat_rpos");    }
-void emit_sm_pat_tab    () { emit_sm_rtcall("PAT_TAB",     "rt_pat_tab");     }
-void emit_sm_pat_rtab   () { emit_sm_rtcall("PAT_RTAB",    "rt_pat_rtab");    }
-void emit_sm_pat_arbno  () { emit_sm_rtcall("PAT_ARBNO",   "rt_pat_arbno");   }
-void emit_sm_pat_cat    () { emit_sm_rtcall("PAT_CAT",     "rt_pat_cat");     }
-void emit_sm_pat_alt    () { emit_sm_rtcall("PAT_ALT",     "rt_pat_alt");     }
-void emit_sm_pat_deref  () { emit_sm_rtcall("PAT_DEREF",   "rt_pat_deref");   }
+void emit_sm_pat_eps()    { emit_macro_begin("PAT_EPS", NULL, 0); bb_emit_call_sym_plt("rt_pat_eps", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_arb()    { emit_macro_begin("PAT_ARB", NULL, 0); bb_emit_call_sym_plt("rt_pat_arb", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_rem()    { emit_macro_begin("PAT_REM", NULL, 0); bb_emit_call_sym_plt("rt_pat_rem", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_fail()   { emit_macro_begin("PAT_FAIL", NULL, 0); bb_emit_call_sym_plt("rt_pat_fail", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_succeed(){ emit_macro_begin("PAT_SUCCEED", NULL, 0); bb_emit_call_sym_plt("rt_pat_succeed", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_abort()  { emit_macro_begin("PAT_ABORT", NULL, 0); bb_emit_call_sym_plt("rt_pat_abort", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_bal()    { emit_macro_begin("PAT_BAL", NULL, 0); bb_emit_call_sym_plt("rt_pat_bal", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_fence()  { emit_macro_begin("PAT_FENCE", NULL, 0); bb_emit_call_sym_plt("rt_pat_fence", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_fence1() { emit_macro_begin("PAT_FENCE1", NULL, 0); bb_emit_call_sym_plt("rt_pat_fence1", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_span()   { emit_macro_begin("PAT_SPAN", NULL, 0); bb_emit_call_sym_plt("rt_pat_span", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_break()  { emit_macro_begin("PAT_BREAK", NULL, 0); bb_emit_call_sym_plt("rt_pat_break", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_any()    { emit_macro_begin("PAT_ANY", NULL, 0); bb_emit_call_sym_plt("rt_pat_any", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_notany() { emit_macro_begin("PAT_NOTANY", NULL, 0); bb_emit_call_sym_plt("rt_pat_notany", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_len()    { emit_macro_begin("PAT_LEN", NULL, 0); bb_emit_call_sym_plt("rt_pat_len", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_pos()    { emit_macro_begin("PAT_POS", NULL, 0); bb_emit_call_sym_plt("rt_pat_pos", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_rpos()   { emit_macro_begin("PAT_RPOS", NULL, 0); bb_emit_call_sym_plt("rt_pat_rpos", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_tab()    { emit_macro_begin("PAT_TAB", NULL, 0); bb_emit_call_sym_plt("rt_pat_tab", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_rtab()   { emit_macro_begin("PAT_RTAB", NULL, 0); bb_emit_call_sym_plt("rt_pat_rtab", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_arbno()  { emit_macro_begin("PAT_ARBNO", NULL, 0); bb_emit_call_sym_plt("rt_pat_arbno", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_cat()    { emit_macro_begin("PAT_CAT", NULL, 0); bb_emit_call_sym_plt("rt_pat_cat", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_alt()    { emit_macro_begin("PAT_ALT", NULL, 0); bb_emit_call_sym_plt("rt_pat_alt", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_pat_deref()  { emit_macro_begin("PAT_DEREF", NULL, 0); bb_emit_call_sym_plt("rt_pat_deref", 0); emit_macro_end(); emit_pad_to_blob_size(); }
 /*====================================================================================================================*/
-void emit_sm_push_var  (const char *lbl, uint64_t ptr) { emit_sm_lbl_rt("PUSH_VAR",  "rt_nv_get", lbl, ptr); }
-void emit_sm_store_var (const char *lbl, uint64_t ptr) { emit_sm_lbl_rt("STORE_VAR", "rt_nv_set", lbl, ptr); }
+void emit_sm_push_var(const char *lbl, uint64_t ptr)  { static const char *const p[] = {"lbl"}; emit_macro_begin("PUSH_VAR", p, 1); emit_lea_rdi_strtab_sym(lbl, ptr); bb_emit_call_sym_plt("rt_nv_get", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_store_var(const char *lbl, uint64_t ptr) { static const char *const p[] = {"lbl"}; emit_macro_begin("STORE_VAR", p, 1); emit_lea_rdi_strtab_sym(lbl, ptr); bb_emit_call_sym_plt("rt_nv_set", 0); emit_macro_end(); emit_pad_to_blob_size(); }
 /*====================================================================================================================*/
-void emit_sm_concat         () { emit_sm_rtcall("CONCAT",         "rt_concat");          }
-void emit_sm_push_null      () { emit_sm_rtcall("PUSH_NULL",      "rt_push_null");       }
-void emit_sm_push_null_noflip(){ emit_sm_rtcall("PUSH_NULL_NOFLIP","rt_push_null_noflip");}
-void emit_sm_coerce_num     () { emit_sm_rtcall("COERCE_NUM",     "rt_coerce_num");      }
-void emit_sm_exp            () { emit_sm_rtcall("EXP_NUM",        "rt_exp");             }
-void emit_sm_neg            () { emit_sm_rtcall("NEGATE",         "rt_neg");             }
-void emit_sm_define         () { emit_sm_rtcall("DEFINE",         "rt_define");          }
-void emit_sm_define_entry   () { emit_sm_rtcall("DEFINE_ENTRY",   "rt_define_entry");    }
+void emit_sm_concat()         { emit_macro_begin("CONCAT", NULL, 0); bb_emit_call_sym_plt("rt_concat", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_push_null()      { emit_macro_begin("PUSH_NULL", NULL, 0); bb_emit_call_sym_plt("rt_push_null", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_push_null_noflip(){ emit_macro_begin("PUSH_NULL_NOFLIP", NULL, 0); bb_emit_call_sym_plt("rt_push_null_noflip", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_coerce_num()     { emit_macro_begin("COERCE_NUM", NULL, 0); bb_emit_call_sym_plt("rt_coerce_num", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_exp()            { emit_macro_begin("EXP_NUM", NULL, 0); bb_emit_call_sym_plt("rt_exp", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_neg()            { emit_macro_begin("NEGATE", NULL, 0); bb_emit_call_sym_plt("rt_neg", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_define()         { emit_macro_begin("DEFINE", NULL, 0); bb_emit_call_sym_plt("rt_define", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_define_entry()   { emit_macro_begin("DEFINE_ENTRY", NULL, 0); bb_emit_call_sym_plt("rt_define_entry", 0); emit_macro_end(); emit_pad_to_blob_size(); }
 /*====================================================================================================================*/
-void emit_sm_resume         () { emit_sm_rtcall("RESUME",         "rt_unhandled_sm"); }
-void emit_sm_suspend        () { emit_sm_rtcall("SUSPEND",        "rt_unhandled_sm"); }
-void emit_sm_suspend_value  () { emit_sm_rtcall("SUSPEND_VALUE",  "rt_unhandled_sm"); }
-void emit_sm_gen_tick       () { emit_sm_rtcall("GEN_TICK",       "rt_unhandled_sm"); }
-void emit_sm_load_glocal    () { emit_sm_rtcall("LOAD_GLOCAL",    "rt_unhandled_sm"); }
-void emit_sm_store_glocal   () { emit_sm_rtcall("STORE_GLOCAL",   "rt_unhandled_sm"); }
-void emit_sm_load_frame     () { emit_sm_rtcall("LOAD_FRAME",     "rt_unhandled_sm"); }
-void emit_sm_store_frame    () { emit_sm_rtcall("STORE_FRAME",    "rt_unhandled_sm"); }
-void emit_sm_icmp_gt        () { emit_sm_rtcall("ICMP_GT",        "rt_unhandled_sm"); }
-void emit_sm_icmp_lt        () { emit_sm_rtcall("ICMP_LT",        "rt_unhandled_sm"); }
-void emit_sm_bb_once        () { emit_sm_rtcall("BB_ONCE",        "rt_unhandled_sm"); }
-void emit_sm_bb_once_proc   () { emit_sm_rtcall("BB_ONCE_PROC",   "rt_unhandled_sm"); }
-void emit_sm_bb_pump        () { emit_sm_rtcall("BB_PUMP",        "rt_unhandled_sm"); }
-void emit_sm_bb_pump_case   () { emit_sm_rtcall("BB_PUMP_CASE",   "rt_unhandled_sm"); }
-void emit_sm_bb_pump_every  () { emit_sm_rtcall("BB_PUMP_EVERY",  "rt_unhandled_sm"); }
-void emit_sm_bb_pump_proc   () { emit_sm_rtcall("BB_PUMP_PROC",   "rt_unhandled_sm"); }
-void emit_sm_bb_pump_sm     () { emit_sm_rtcall("BB_PUMP_SM",     "rt_unhandled_sm"); }
-void emit_sm_bb_pump_ast    () { emit_sm_rtcall("BB_PUMP_AST",    "rt_bb_pump_ast"); }
+void emit_sm_resume()         { emit_macro_begin("RESUME", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_suspend()        { emit_macro_begin("SUSPEND", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_suspend_value()  { emit_macro_begin("SUSPEND_VALUE", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_gen_tick()       { emit_macro_begin("GEN_TICK", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_load_glocal()    { emit_macro_begin("LOAD_GLOCAL", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_store_glocal()   { emit_macro_begin("STORE_GLOCAL", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_load_frame()     { emit_macro_begin("LOAD_FRAME", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_store_frame()    { emit_macro_begin("STORE_FRAME", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_icmp_gt()        { emit_macro_begin("ICMP_GT", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_icmp_lt()        { emit_macro_begin("ICMP_LT", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_once()        { emit_macro_begin("BB_ONCE", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_once_proc()   { emit_macro_begin("BB_ONCE_PROC", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_pump()        { emit_macro_begin("BB_PUMP", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_pump_case()   { emit_macro_begin("BB_PUMP_CASE", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_pump_every()  { emit_macro_begin("BB_PUMP_EVERY", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_pump_proc()   { emit_macro_begin("BB_PUMP_PROC", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_pump_sm()     { emit_macro_begin("BB_PUMP_SM", NULL, 0); bb_emit_call_sym_plt("rt_unhandled_sm", 0); emit_macro_end(); emit_pad_to_blob_size(); }
+void emit_sm_bb_pump_ast()    { emit_macro_begin("BB_PUMP_AST", NULL, 0); bb_emit_call_sym_plt("rt_bb_pump_ast", 0); emit_macro_end(); emit_pad_to_blob_size(); }
 /*====================================================================================================================*/
 void emit_sm_unhandled_op(int op)
 {
@@ -330,31 +324,4 @@ void emit_sm_unhandled_op(int op)
     emit_pad_to_blob_size();
 }
 /*====================================================================================================================*/
-static void emit_sm_rtcall(const char *macro, const char *rt_sym)
-{
-    emit_macro_begin(macro, NULL, 0);
-    bb_emit_call_sym_plt(rt_sym, 0);
-    emit_macro_end();
-    emit_pad_to_blob_size();
-}
-static void emit_sm_lbl_rt(const char *macro, const char *rt_sym,
-                             const char *name_lbl, uint64_t name_ptr)
-{
-    static const char *const params[] = { "lbl" };
-    emit_macro_begin(macro, params, 1);
-    emit_lea_rdi_strtab_sym(name_lbl, name_ptr);
-    bb_emit_call_sym_plt(rt_sym, 0);
-    emit_macro_end();
-    emit_pad_to_blob_size();
-}
-static void emit_sm_pat_lbl_rt(const char *macro, const char *rt_sym,
-                                 const char *name_lbl, uint64_t name_ptr)
-{
-    static const char *const params[] = { "lbl" };
-    emit_macro_begin(macro, params, 1);
-    emit_lea_rdi_strtab_sym(name_lbl, name_ptr);
-    bb_emit_call_sym_plt(rt_sym, 0);
-    emit_macro_end();
-    emit_pad_to_blob_size();
-}
 /*====================================================================================================================*/
